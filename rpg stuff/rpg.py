d7cf07a791d946548cb3839efb8511914ed5a68c
Pyraq <dominik56000@gmail.com>
2019-01-17 18:48:44 +0100

    The level up system now works, and the game can be completed.

--- rpg stuff/rpg.py
+++ rpg stuff/rpg.py
@@ -12,12 +12,15 @@ EATK = 0
 EXPGain = 0
 EnemyChoice = '0'
 
-def CharacterChoice(): #Class choice function
+#Class choice function
+def CharacterChoice(): 
 	global PATK
 	global PDEF
 	global PHP
+	global PHPActive
 	print('Choose your class:')
-	print('BERSERKER (ATK = 2, DEF = 1, HP = 15), PROTECTOR (ATK = 1, DEF = 2, HP = 20)')
+	ClassInfo = 'BERSERKER (ATK = 2, DEF = 1, HP = 15), PROTECTOR (ATK = 1, DEF = 2, HP = 15)'
+	print(ClassInfo)
 	CChoice = True
 	while CChoice == True:
 		ClassChoice = input()
@@ -25,23 +28,28 @@ def CharacterChoice(): #Class choice function
 			PATK = 2
 			PDEF = 1
 			PHP = 15
+			PHPActive = 15
 			CChoice = False
 		elif ClassChoice == 'PROTECTOR':
 			PATK = 1
 			PDEF = 2
 			PHP = 15
+			PHPActive = 15
 			CChoice = False
 		else:
 			print('Please input the right class name:')
-	return PATK and PDEF and PHP
+			print(ClassInfo)
+	return PATK and PDEF and PHP and PHPActive
 
-def BadPlayerChoice(): #Enemy Choice function
+#Enemy Choice function
+def BadPlayerChoice(): 
 	global EATK
 	global EHP
 	global EXPGain
 	global EnemyChoice
 	print('Choose an enemy to fight:')
-	print('RAT, GOBLIN')
+	EnemyInfo = 'RAT (ATK = 1, EXP = 5, HP = 7), GOBLIN (ATK = 2, EXP = 10, HP = 15)'
+	print(EnemyInfo)
 	EChoice = True
 	while EChoice == True:
 		EnemyChoice = input()
@@ -57,17 +65,23 @@ def BadPlayerChoice(): #Enemy Choice function
 			EChoice = False
 		else:
 			print('Please input the right enemy name:')
+			print(EnemyInfo)
 	return EATK and EHP and EXPGain and EnemyChoice
 
-def FightPhase(): #Fight function
+#Fight function
+def FightPhase(): 
+	global LVL
 	global EHP
 	global PHP
+	global PHPActive
+	global PATK
+	global PDEF
 	global EXP
 	while EHP > 0:
 		if PHP <= 0:
 			print('You died!')
 			break
-		print('What should I do? Current Enemy: ' + EnemyChoice + ' Enemy HP: ' + str(EHP) + ' Your HP: ' + str(PHP))
+		print('What should I do? Current Enemy: ' + EnemyChoice + ', Enemy HP: ' + str(EHP) + ', Your HP: ' + str(PHPActive) + '/' + str(PHP))
 		print('ATTACK, DEFEND')
 		PATKCALC = 0 #Your Attack value
 		PDEFCALC = 0 #Your Defense value
@@ -77,37 +91,60 @@ def FightPhase(): #Fight function
 			PATKCALC = random.randint(0, PATK)
 			EATKCALC = random.randint(0, EATK)
 			if PATKCALC == 0:
-				PHP = PHP - EATKCALC
+				PHPActive = PHPActive - EATKCALC
 				print('You missed your attack, and the enemy hit you for ' + str(EATKCALC) + ' damage!')
 			else:
 				EHP = EHP - PATKCALC
 				print('You hit your an enemy, and dealt ' + str(PATKCALC) + ' damage!')
-				PHP = PHP - EATKCALC
+				PHPActive = PHPActive - EATKCALC
 				print('The enemy dealt ' + str(EATKCALC) + ' damage!')
 		elif FightChoice == 'DEFEND': #defend option
 			PDEFCALC = random.randint(0, PDEF)
 			EATKCALC = random.randint(0, EATK)
 			if PDEFCALC == 0:
-				PHP = PHP - EATKCALC
+				PHPActive = PHPActive - EATKCALC
 				print('You were too slow, and the enemy hit you for ' + str(EATKCALC) + ' damage!')
 			else:
 				if PDEFCALC > EATKCALC:
 					PDEFCALC = EATKCALC
-					PHP = PHP - (EATKCALC - PDEFCALC)
+					PHPActive = PHPActive - (EATKCALC - PDEFCALC)
 					print('You defended against an enemy, and deflected ' + str(PDEFCALC) + ' damage!')
 				else:
-					PHP = PHP - (EATKCALC - PDEFCALC)
+					PHPActive = PHPActive - (EATKCALC - PDEFCALC)
 					print('You defended against an enemy, and deflected ' + str(PDEFCALC) + ' damage!')
 		else:
-			print('Please input the right command:')
+			print('Please input the right command.')
 	else:
 		EXP = EXP + EXPGain
 		print('You won the fight! You gained ' + str(EXPGain) + ' EXP! Your current EXP = ' + str(EXP + LVLRequirements) + '.')
-		if EXP >= 0:
+		if EXP >= 0: #Level up if statement
 			LVL = LVL + 1
 			EXP = EXP - LVLRequirements
 			print('You leveled up! You are now LVL ' + str(LVL) + '!')
+			print('You can upgrade your stats, please input one of them:')
+			LVLUPInfo = 'ATK, DEF, HP'
+			print(LVLUPInfo)
+			LUChoice = True
+			while LUChoice == True:
+				LVLUPChoice = input()
+				if LVLUPChoice == 'ATK':
+					PATK = PATK + 1
+					LUChoice = False
+				elif LVLUPChoice == 'DEF':
+					PDEF = PDEF + 1
+					LUChoice = False
+				elif LVLUPChoice == 'HP':
+					PHP = PHP + 2
+					LUChoice = False
+				else:
+					print('Please input the right stat name:')
+					print(LVLUPInfo)
+		PHPActive = PHP
 
 CharacterChoice()
-BadPlayerChoice()
-FightPhase()
+while LVL < 5:
+	BadPlayerChoice()
+	FightPhase()
+
+if LVL == 5:
+	print('You won the game!')
